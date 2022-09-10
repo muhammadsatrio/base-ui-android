90188753ec412f3387cf1adab477e059b2707f8c
Satrio <muhammad.satrio@gojek.com>
2022-09-10 09:11:18 +0700

    add effect button clicked

--- app/src/main/java/com/satrio/baseuiandroid/SatrioButton.kt
+++ app/src/main/java/com/satrio/baseuiandroid/SatrioButton.kt
@@ -1,14 +1,23 @@
 package com.satrio.baseuiandroid
 
+import android.animation.Animator
+import android.animation.AnimatorSet
+import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.drawable.GradientDrawable
 import android.graphics.drawable.LayerDrawable
 import android.graphics.drawable.StateListDrawable
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
 import android.widget.FrameLayout
+import android.widget.LinearLayout
 import androidx.annotation.DrawableRes
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.view.updateLayoutParams
 import com.satrio.baseuiandroid.databinding.SatrioButtonBinding
 
 class SatrioButton(context: Context, attributeSet: AttributeSet) :
@@ -20,6 +29,7 @@ class SatrioButton(context: Context, attributeSet: AttributeSet) :
         setButtonBackground(R.drawable.button_background)
         setButtonText(context, attributeSet)
         setMargin()
+        setTouchListener()
         binding.buttonContainer.isClickable = true
     }
 
@@ -27,6 +37,38 @@ class SatrioButton(context: Context, attributeSet: AttributeSet) :
         binding.buttonContainer.setOnClickListener {
             function?.invoke()
         }
+    }
+
+    private fun setTouchListener() {
+        binding.buttonContainer.setOnTouchListener { view, motionEvent ->
+            when (motionEvent.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    doActionClickDown()
+                }
+                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                    doActionClickUp()
+                }
+            }
+            true
+        }
+    }
+
+    private fun doActionClickDown() {
+        val animatorList = mutableListOf<Animator>()
+        animatorList.add(getScaleAnimator(binding.buttonContainer, 1f, 0.9f))
+
+        AnimatorSet().apply {
+            playTogether(animatorList)
+        }.start()
+    }
+
+    private fun doActionClickUp() {
+        val animatorList = mutableListOf<Animator>()
+        animatorList.add(getScaleAnimator(binding.buttonContainer, 0.9f, 1f))
+
+        AnimatorSet().apply {
+            playTogether(animatorList)
+        }.start()
     }
 
     private fun setMargin() {
@@ -37,6 +79,7 @@ class SatrioButton(context: Context, attributeSet: AttributeSet) :
             this.rightMargin = 20
         }
     }
+
     private fun setButtonText(context: Context, attributeSet: AttributeSet) {
         val typedArray =
             context.obtainStyledAttributes(attributeSet, R.styleable.SatrioButton, 0, 0)
@@ -63,17 +106,17 @@ class SatrioButton(context: Context, attributeSet: AttributeSet) :
         binding.buttonContainer.background = stateListDrawable
     }
 
-//    private fun getScaleAnimator(
-//        view: View,
-//        from: Float,
-//        to: Float
-//    ): Animator {
-//        return ValueAnimator.ofFloat(from, to).apply {
-//            duration = 33
-//            addUpdateListener {
-//                view.scaleX = (it.animatedValue as Float)
-//                view.scaleY = (it.animatedValue as Float)
-//            }
-//        }
-//    }
+    private fun getScaleAnimator(
+        view: View,
+        from: Float,
+        to: Float
+    ): Animator {
+        return ValueAnimator.ofFloat(from, to).apply {
+            duration = 33
+            addUpdateListener {
+                view.scaleX = (it.animatedValue as Float)
+                view.scaleY = (it.animatedValue as Float)
+            }
+        }
+    }
 }
